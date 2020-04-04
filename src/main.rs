c87c7c8e8c8e3d4a062998ad62c614e8fd02ba48
Colibre <colibre5@gmail.com>
2020-04-04 15:00:49 +0200

    befnode

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,82 @@
 
 
-//const nominaly: [u32; 3] = [2, 5, 10];
 
 fn main() {
     let val = 17;
     let combinations: Vec<Vec<u32>> = vec![];
 
+    let nominals = [10, 5, 2];
+
+
     let temp: Vec<u32> = vec![];
-    loop {
-        let combination = vec![];
-        let n = val;
+    'main: loop {
+        let mut combination = vec![];
+        let mut n = val;
+        //let mut res = step(10, combination, n);
+        let mut current_nominal = nominals.iter().peekable();
         loop {
-            let res = step()
+            match step(**current_nominal.peek().unwrap(), &mut combination, n) {
+                Step::Continue((Some(combination), remainder)) => {
+                    n = remainder;
+                    println!("{}: {:?}", remainder, combination);
+                },
+                Step::Continue((None, remainder)) => {
+
+                },
+                Step::DowngradeNominal(nominal) => {
+                    current_nominal.next();
+                    println!("lol");
+                },
+                Step::Impossible => {
+                    break 'main;
+                }
+            }
         }
     }
 
     println!("Hello, world!");
 }
 
-fn step(nominaly: Vec<u32>, var: Vec<u32>, num: u32) -> Option<(Option<Vec<u32>>, u32)> {
+fn step(nom: u32, sumof: &mut Vec<u32>, mut num: u32) -> Step {
     if num == 0 {
-        return None;
+        return Step::Impossible;
     }
-    let last_nominal = var.last().or(Some(&std::u32::MAX)).unwrap();
-    for n in nominaly.iter() {
-        if n > last_nominal { return Some((None, *n)); /* znak aby usunąć ten nominał - dla tego nominału nie ma następnego kroku */ } else
-        if n % var.last()? == 0 {
-            num = num - n;
-            var.push(*n);
-        }
-        
+
+    if num.checked_sub(nom).is_some() {
+        num = num - nom;
+        sumof.push(nom);
+    } else {
+        return Step::DowngradeNominal(nom);
     }
-    Some((Some(var), 2))
+    
+    Step::Continue((Some(sumof.to_vec()), num))
 }
 
 //10 5 2
 //5 5 5 2
 //5 2 2 2 2 2 2
+#[derive(Debug)]
+enum Step {
+    Continue((Option<Vec<u32>>, u32)),
+    DowngradeNominal(u32),
+    //CannotProceed,
+    //BranchLower((Option<Vec<u32>>, u32)),
+    Impossible,
+}
+
+struct Node<T> {
+    value: T,
+    nodes: Vec<Node<T>>,
+}
+
+impl<T> Node<T> {
+    fn new(value: T) -> Self {
+        Node {
+            value,
+            nodes: vec![],
+        }
+    }
+    fn add_node(&mut self, node: Node<T>) {
+        self.nodes.push(node);
+    }
+}
